0199e3a3f5cac36ea77eda35686537bc77fac078
RezaBanivakil <rezabaniv@gmail.com>
2025-04-11 09:49:35 +0330

    0.1.15
    section 15 and quiz 3 completed

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -5,7 +5,11 @@ trait AppendBar {
 }
 
 impl AppendBar for String {
-    // TODO: Implement `AppendBar` for the type `String`.
+    fn append_bar(self) -> Self {
+        let mut value = self;
+        value.push_str("Bar");
+        value
+    }
 }
 
 fn main() {
